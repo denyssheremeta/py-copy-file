1da9788ddc67c47de488ef6833f29a9c94170876
Denys Sheremeta <DSheremeta@franpos.com>
2025-06-17 21:09:28 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ def copy_file(command: str) -> None:
     if len(parts) != 3 or parts[0] != "cp":
         return
 
-    _, source_file, destination_file = parts
+    _, source, destination = parts
 
-    if source_file == destination_file:
+    if source == destination:
         return
 
     try:
-        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
             file_out.write(file_in.read())
     except FileNotFoundError:
         return
